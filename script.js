9e7f8893a4a3146b65cf2911e299f94865431872
Shashank Talwar <66951104+Shashank8175@users.noreply.github.com>
2022-04-28 18:12:24 +0530

    remove unuseful comments.

--- script.js
+++ script.js
@@ -48,10 +48,4 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-// const playerSelection = "rock";
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
-
-
 game();
